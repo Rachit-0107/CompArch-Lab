efa5920e752ab9d07e34dff36416b93b1f62e918
Rachit Agrawal <80877174+Rachit-0107@users.noreply.github.com>
2023-02-10 17:39:45 +0530

    Update SingleCycle.v

--- Lab6/SingleCycle.v
+++ Lab6/SingleCycle.v
@@ -1,3 +1,14 @@
+module PC(count, clock, reset);
+  input		clock, reset;
+  output	[31:0]	count;
+  reg	[31:0]	count;
+  always @(posedge clock)
+    if(!reset)
+      count = count + 1;
+    else
+      count = 0;
+endmodule
+
 module Instruction_Memory(Inst, PC, clock);
 	input[31:0] PC;
 	input clock;
@@ -60,6 +71,43 @@ module DECODER(out, x, y, z);
   and g9(out[5], x, y0, z);
   and g10(out[6], x, y, z0);
   and g11(out[7], x, y, z);
+endmodule 
+
+module decoder5_32(Out, In);
+  input [4:0] In;
+  output  [31:0] Out;
+  assign  Out[0] = (~In[4] & ~In[3] & ~In[2] & ~In[1] & ~In[0]),
+          Out[1] = (~In[4] & ~In[3] & ~In[2] & ~In[1] & In[0]),
+          Out[2] = (~In[4] & ~In[3] & ~In[2] & In[1] & ~In[0]),
+          Out[3] = (~In[4] & ~In[3] & ~In[2] & In[1] & In[0]),
+          Out[4] = (~In[4] & ~In[3] & In[2] & ~In[1] & ~In[0]),
+          Out[5] = (~In[4] & ~In[3] & In[2] & ~In[1] & In[0]),
+          Out[6] = (~In[4] & ~In[3] & In[2] & In[1] & ~In[0]),
+          Out[7] = (~In[4] & ~In[3] & In[2] & In[1] & In[0]),
+          Out[8] = (~In[4] & In[3] & ~In[2] & ~In[1] & ~In[0]),
+          Out[9] = (~In[4] & In[3] & ~In[2] & ~In[1] & In[0]),
+          Out[10] = (~In[4] & In[3] & ~In[2] & In[1] & ~In[0]),
+          Out[11] = (~In[4] & In[3] & ~In[2] & In[1] & In[0]),
+          Out[12] = (~In[4] & In[3] & In[2] & ~In[1] & ~In[0]),
+          Out[13] = (~In[4] & In[3] & In[2] & ~In[1] & In[0]),
+          Out[14] = (~In[4] & In[3] & In[2] & In[1] & ~In[0]),
+          Out[15] = (~In[4] & In[3] & In[2] & In[1] & In[0]),
+          Out[16] = (In[4] & ~In[3] & ~In[2] & ~In[1] & ~In[0]),
+          Out[17] = (In[4] & ~In[3] & ~In[2] & ~In[1] & In[0]),
+          Out[18] = (In[4] & ~In[3] & ~In[2] & In[1] & ~In[0]),
+          Out[19] = (In[4] & ~In[3] & ~In[2] & In[1] & In[0]),
+          Out[20] = (In[4] & ~In[3] & In[2] & ~In[1] & ~In[0]),
+          Out[21] = (In[4] & ~In[3] & In[2] & ~In[1] & In[0]),
+          Out[22] = (In[4] & ~In[3] & In[2] & In[1] & ~In[0]),
+          Out[23] = (In[4] & ~In[3] & In[2] & In[1] & In[0]),
+          Out[24] = (In[4] & In[3] & ~In[2] & ~In[1] & ~In[0]),
+          Out[25] = (In[4] & In[3] & ~In[2] & ~In[1] & In[0]),
+          Out[26] = (In[4] & In[3] & ~In[2] & In[1] & ~In[0]),
+          Out[27] = (In[4] & In[3] & ~In[2] & In[1] & In[0]),
+          Out[28] = (In[4] & In[3] & In[2] & ~In[1] & ~In[0]),
+          Out[29] = (In[4] & In[3] & In[2] & ~In[1] & In[0]),
+          Out[30] = (In[4] & In[3] & In[2] & In[1] & ~In[0]),
+          Out[31] = (In[4] & In[3] & In[2] & In[1] & In[0]);
 endmodule
 
 module  FADDER(carry, sum, x, y, z);
